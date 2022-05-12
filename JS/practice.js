c6bc50ed1e6888857eadb2bd87d36f47beeb7a01
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-12 16:52:29 -0400

    created table in js to render film properties on the html

--- JS/practice.js
+++ JS/practice.js
@@ -2,3 +2,30 @@ var table = $("#table tbody");
 $.each(data, function(idx, elem){
     table.append("<tr><td>"+elem.poster+"</td><td>"+elem.title+"</td><td>"+elem.rating+"</td><td>"+elem.button+"</td></tr>");
 });
+
+
+
+
+let html = '';
+movies.forEach(movie => {
+    html += `<table class="movie">
+            table.append(
+            "<tr>
+            <td><img class="poster" src="${movie.poster}" alt="a movie poster"></td>
+            <td>${movie.title}</td>
+            <td>${movie.rating}</td>
+            <td><button type="button" class="delete" id="${movie.id}">Delete</button></td>
+            </tr>");
+           </table>
+                    <br>`
+});
+
+
+$('#movies').replaceWith(html)
+
+
+
+// var table = $("#movies tbody");
+// $.each(data, function(idx, movie){
+//     table.append("<tr><td>"+movie.poster+"</td><td>"+movie.title+"</td><td>"+movie.rating+"</td><td>"+movie.id+"</td></tr>");
+// });
