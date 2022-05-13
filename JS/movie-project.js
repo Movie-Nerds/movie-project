9242960220d84d810122fa395d379f646fefd48a
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-13 10:51:40 -0400

    added edit button functionality and updated styling for html table

--- JS/movie-project.js
+++ JS/movie-project.js
@@ -1,24 +1,27 @@
 "use strict";
 
-const URL = "https://flint-capable-earthquake.glitch.me/movies";
+const URL = "https://boiling-hissing-moonflower.glitch.me/movies";
 
+// *** RENDER MOVIES ***
+let html = '';
 const displayMovies = () => {
     return fetch(URL)
         .then(res => res.json())
         .then(movies => {
             console.log(movies);
-            // $('#loading').replaceWith('');
-            let html = '';
+            $('#loading').replaceWith('');
             movies.forEach(movie => {
-                html += `<div class="movie">
-                        <img class="poster" src="${movie.poster}" alt="a movie poster">
-                        <h5 contenteditable="true" id="edit">${movie.title}</h5>
-                        <h6 contenteditable="true" id="edit">Rating: ${movie.rating}</h6>
-                        <button type="button" class="delete" id="${movie.id}">Delete</button>
-                    </div>
+                html += `<tbody class="movie">
+                        <tr>
+           <th scope="row"><img class="poster" src="${movie.poster}" alt="a movie poster"</th>
+            <td><h5 contenteditable="true" id="edit">${movie.title}</h5></td>
+            <td><h6 contenteditable="true" id="edit">⭐️ ${movie.rating}</h6></td>
+            <td><button type="button" class="delete" id="${movie.id}">Delete</button></td>
+            </tr>
+                    </tbody>
                     <br>`
             });
-            $('#movies').replaceWith(html)
+            $('#movies-list').replaceWith(html)
         })
         .catch(error => {
             console.log(error);
@@ -26,6 +29,42 @@ const displayMovies = () => {
 }
 displayMovies();
 
+// *** POST FUNCTION ***
+$('#add-new-movie').click(function(e) {
+    e.preventDefault();
+    const moviePost = {id: '', title: $('#title').val(), rating: $('#rating').val()};
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(moviePost),
+    };
+    fetch(URL, options)
+        .then(res => res.json())
+        .then(newMovie => {
+            console.log(newMovie);
+            html = `<div class="movie">
+                        <h5>${newMovie.title}</h5>
+                        <h6>Rating: ${newMovie.rating}</h6>
+                    </div>
+                    <br>`
+            $('#movie').append(html);
+        });
+    setTimeout(function (){
+        location.reload();
+    },1000);
+
+});
+
+// *** DELETE BUTTON FUNCTIONALITY ***
+// const deleteMovie = (movieID) => {
+//     let options = {
+//         method: 'DELETE',
+//         headers: {'Content-Type': 'application/json'},
+//     }
+//     return fetch(`${URL}/${movieID}`, options).then(displayMovies).catch(err => console.error(err));
+// }
 function deleteMovie(id) {
     fetch(`${URL}/${id}`, {
         method: 'DELETE',
@@ -39,14 +78,61 @@ function deleteMovie(id) {
         })
         .catch(console.error)
 }
-
-// *** DELETE BUTTON FUNCTIONALITY ***
-
 let timeoutForDelete = setTimeout(function() {
     $('.delete').click(function(e) {
         e.preventDefault();
         var id = $(e.target).attr('id');
         deleteMovie(id);
-        // location.reload(true);
+        setTimeout(function (){
+            location.reload();
+        },1000);
     });
 }, 1000);
+
+//Edit button function
+
+
+const editMovie = (movie) => fetch(`${URL}/${movie.id}`, {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(movie)
+})
+    .then(res => res.json())
+    .then(data => {
+        console.log(`Success: edited ${JSON.stringify(data)}`);
+    });
+
+fetch(URL, editMovie)
+    .then(response => response.json())
+    .then(editPost => {
+        console.log(editPost);
+        html += `<div class="movies" id="${editPost.id}">
+                        <h5 contenteditable="true">${editPost.title}</h5>
+                        <h6 contenteditable="true">Rating: ${editPost.rating}</h6>
+                    </div>
+                    <br>`
+        $('').replaceWith(html);
+    });
+
+$("#update").click(function(e) {
+    e.preventDefault();
+    // let id = $(e.target).attr('id')
+    // alert("clicked!");
+    // editMovie(id)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
